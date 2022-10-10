a6c7431b00963d39124c3382ac86ecaf24ee2d72
Pranavnarkhede <114733916+Pranavnarkhede@users.noreply.github.com>
2022-10-10 15:59:14 +0530

    Create m1.c

---
+++ m1.c
@@ -0,0 +1,132 @@
+int main(){
+    int a[3][3],b[3][3],c[3][3],d[3][3]={0},ch,i,j,k,sum=0,min,max,flag=0,s[2][2];
+    int *p,*q;
+    p=&a[0][0];
+    q=&b[0][0];
+    printf("enter A array elements:\n");
+    for( i=0;i<3;i++){
+        for( j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("enter B array elements:\n");
+    for( i=0;i<3;i++){
+        for( j=0;j<3;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+   aa: printf("Enter 1 for addition\n");
+    printf("Enter 2 for multiplication\n");
+    printf("Enter 3 for sadddle point\n");
+    printf("Enter 4 for saddle point using poiunter\n");
+    printf("Enter 5 for exit\n");
+    scanf(" %d",&ch);
+    
+       switch(ch){
+        case 1:
+        for( i=0;i<3;i++){
+            for( j=0;j<3;j++){
+                c[i][j]=*(p+i*3+j)+*(q+i*3+j);
+            }
+        }
+        printf("additon of matrix A and B is :\n");
+        for( i=0;i<3;i++){
+            for( j=0;j<3;j++){
+                printf("%d \t",c[i][j]);
+            }printf("\n");
+        
+        }
+        break;
+        case 2:
+        for( i=0;i<3;i++){
+            for( j=0;j<3;j++){
+                for(k=0;k<3;k++){
+                    sum+=(*(p+i*3+k))*(*(q+k*3+j));
+                }d[i][j]=sum;
+                sum=0;
+            }
+        }
+         printf("multiplication of matrix A and B is :\n");
+        for( i=0;i<3;i++){
+            for( j=0;j<3;j++){
+                printf("%d \t",d[i][j]);
+            }printf("\n");
+        }
+        break;
+        case 3:
+         for(i=0;i<3;i++)
+   {
+       min=a[i][0];
+       for(j=0;j<3;j++)
+       {
+           if(min>=a[i][j])
+           {
+               min=a[i][j];
+               s[0][0]=i;
+               s[0][1]=j;
+           }
+       }
+       j=s[0][1];
+       max=a[0][j];
+       for(k=0;k<3;k++)
+       {
+           if(max<=a[k][j])
+           {
+               max=a[k][j];
+               s[1][0]=k;
+               s[1][1]=j;
+           }
+       }
+       if(min==max)
+       {
+           if(s[0][0]==s[1][0] && s[0][1]==s[1][1])
+           {
+               printf("Saddle point(%d,%d)=%d",s[0][0],s[0][1],max);
+           }
+       }
+   }
+   break;
+   
+   case 4:
+   
+    for(i=0;i<3;i++)
+   {
+       min=*(p+i*3);
+       for(j=0;j<3;j++)
+       {
+           if(min>=*(p+i*3+j))
+           {
+               min=*(p+i*3+j);
+               s[0][0]=i;
+               s[0][1]=j;
+           }
+       }
+       j=s[0][1];
+       max=*(p+j);
+       for(k=0;k<3;k++)
+       {
+           if(max<=*(p+k*3+j))
+           {
+               max=*(p+k*3+j);
+               s[1][0]=k;
+               s[1][1]=j;
+           }
+       }
+       if(min==max)
+       {
+           if(s[0][0]==s[1][0] && s[0][1]==s[1][1])
+           {
+               printf("Saddle point(%d,%d)=%d",s[0][0],s[0][1],max);
+           }
+       }
+   }
+   break;
+
+
+
+       
+}
+goto aa;
+
+return 0;
+}
