4b3257fd1714956f5c54470823764af0d948772d
Pranavnarkhede <114733916+Pranavnarkhede@users.noreply.github.com>
2022-10-13 20:43:01 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -26,12 +26,12 @@ int main()
 	}
 	do
 	{
-		printf("\n1.Addition\n2.Multiplication\n3.Saddle point");
-		printf("\n Enter your choice=");
+		printf("\n1.Addition\n2.Multiplication\n3.Saddle point using array\n4.Saddle point using pointer\n");
+		printf("\n Enter your choice=\n");
 		scanf("%d",&ch);
 		switch(ch)
 		{
-			case 1:
+	case 1:
             for(i=0;i<3;i++)
 		{
 			for(j=0;j<3;j++)
@@ -52,8 +52,8 @@ int main()
 		
 
             
-				break;
-			case 2:
+	 break;
+	case 2:
 
              for(i=0;i<3;i++)
 		{
@@ -81,8 +81,42 @@ int main()
 	
 
 
-				break;
-			case 3:
+	break;
+       case 3:
+		for(i=0;i<3;i++)
+   {
+       min=a[i][0];
+       for(j=0;j<3;j++)
+       {
+           if(min>=a[i][j])
+           {
+               min=a[i][j];
+               s[0][0]=i;
+               s[0][1]=j;
+           }
+       }
+       j=s[0][1];
+       max=a[0][j];
+       for(k=0;k<3;k++)
+       {
+           if(max<=a[k][j])
+           {
+               max=a[k][j];
+               s[1][0]=k;
+               s[1][1]=j;
+           }
+       }
+       if(min==max)
+       {
+           if(s[0][0]==s[1][0] && s[0][1]==s[1][1])
+           {
+               printf("Saddle point(%d,%d)=%d",s[0][0],s[0][1],max);
+           }
+       }
+   }
+             break;
+
+			case 4:
             for(i=0;i<3;i++)
    {
        min=*(p+(i*3)+0);
@@ -110,7 +144,7 @@ int main()
        {
            if(s[0][0]==s[1][0] && s[0][1]==s[1][1])
            {
-               printf("Saddle point(%d,%d)=%d",s[0][0],s[0][1],max);
+               printf("\nSaddle point(%d,%d)=%d",s[0][0],s[0][1],max);
                flag=1;
            }
        }
@@ -120,86 +154,10 @@ int main()
    {
     printf("no saddle point");
    }
-				break;
+	break;
 			
-			default:printf("\n Invalid choice");
+    default:printf("\n Invalid choice");
 		}
-	}while(ch!=0);
+  }while(ch!=0);
 return 0;
 }
-/*
-int addmat(int *p,int *q,int *r)
-{
-	int i,j;
-	
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{
-				  *(r+(i*3)+j)= *(p+(i*3)+j)+*(q+(i*3)+j);	
-			}
-			
-		}
-		printf("\n sum of matrix=\n");
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{	
-			printf("%d	",*(r+(i*3)+j));
-			}
-			printf("\n");
-		}
-		
-}
-int mul(int *p,int *q,int *r)
-{
-	int i,j,k;
-	 
-     for(i=0;i<3;i++)
-		{
-            *(r+(i*3)+j)=0;
-			for(j=0;j<3;j++)
-			{
-				for(k=0;k<3;k++)
-				{
-					*(r+(i*3)+j)=*(r+(i*3)+j)+*(p+(i*3)+k)*(*(q+(k*3)+j));
-				}
-			}
-		}
-	printf("\n multiplication of matrix=\n");
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{	
-			printf("%d	",*(r+(i*3)+j));
-			}
-			printf("\n");
-		}
-	
-}
-/*int transpose(int *p,int *q)
-{
-	int i=0,j=0;
-	
-
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{
-				*(q+(i*3)+j)=*(p+(j*3)+i);
-				
-			}
-			
-		}
-		printf("\n Transpose of matrix=\n");
-		for(i=0;i<3;i++)
-		{
-			for(j=0;j<3;j++)
-			{	
-			printf("%d	",*(q+(i*3)+j));
-			}
-			printf("\n");
-		}
-}
-
-*/
