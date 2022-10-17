e275c17796ec2eea895a54974fdd73b5af0f79e4
Pranavnarkhede <114733916+Pranavnarkhede@users.noreply.github.com>
2022-10-17 16:00:40 +0530

    Update sparsematrix.c

--- sparsematrix.c
+++ sparsematrix.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   int r,c,count=0,i,j;
+   int r,c,count=0,i,j,k=0,p=0;
    printf("Enter rows an columns of matrix:");
    scanf("%d %d",&r,&c);
    int a[r][c];
@@ -11,7 +11,11 @@ int main() {
        for(j=0;j<c;j++)
        {
            scanf(" %d",&a[i][j]);
-       }
+           if(a[i][j]!=0)
+           {
+               k++;
+           }
+       }   
        printf("\n");
    }
    printf("Matrix A:\n");
@@ -33,13 +37,36 @@ int main() {
    }   
    if(count>((i*j)/2))
    {
-       printf("Matrix A is sparse matrix");
+       printf("Matrix A is sparse matrix\n");
    }
    else
    {
-      printf("Matrix A is not sparse matrix");
+      printf("Matrix A is not sparse matrix\n");
    }
-   
+  int s[k][3];
+  for(i=0;i<r;i++)
+  {
+   for(j=0;j<c;j++)
+   {
+       if(a[i][j]!=0)
+       {
+           s[p][0]=i;
+           s[p][1]=j;
+           s[p][2]=a[i][j];
+           p++;
+       }
+   }
+  }
+  printf("Triplet Reprenstation of Sparse Matrix:\n");
+  for(i=0;i<k;i++)
+  {
+    for(j=0;j<3;j++)
+    {
+       printf("%d \t",s[i][j]);
+    }
+    printf("\n");
+  }
+  
    
     return 0;
 }
