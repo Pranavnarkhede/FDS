f843afcd8f2e24e5f73a4e23d8df4de9168eaac4
Pranavnarkhede <114733916+Pranavnarkhede@users.noreply.github.com>
2022-11-12 22:29:27 +0530

    Update assignmnet7.c

--- assignmnet7.c
+++ assignmnet7.c
@@ -9,31 +9,23 @@ struct abc
     struct abc *next;
     struct abc *prev;
 };
-void insertM(struct abc*);
-void deleteM(struct abc*);
-void deleteP(struct abc*);
-void deleteS(struct abc*);
-void deleteM(struct abc*);
-int count(struct abc*);
-void reverse(struct abc*);
 
+struct abc *head;
 
-
-
-
-void inserts(struct abc* head)
+void inserts()
 {
-    struct abc*new;
+    struct abc*new,*temp;
+    temp=head;
     new=(struct abc*)malloc(sizeof(struct abc));
     printf("enter name and PRN(Secretary) :\n");
     scanf(" %s",new->name);
     scanf("%d",&new->prn);
     new->next=NULL;
     new->prev=NULL;
-    new->prev=head;
-    head->next=new;
+    new->prev=temp;
+    temp->next=new;
 }
-void display(struct abc *head)
+void display()
 {
      struct abc * temp;
      temp=head;
@@ -43,50 +35,11 @@ void display(struct abc *head)
             temp =temp->next;
         }
 }
-
-void main()
-{
-    struct abc*head;
-    head=(struct abc*)malloc(sizeof(struct abc));
-    printf("enter name and PRN(President):\n");
-    scanf(" %s",head->name);
-    scanf("%d",&head->prn);
-    head->next=NULL;
-    head->prev=NULL;
-    inserts(head);
-    printf("\n");
-    int ch,y;
-    do{
-        printf("\n 1.Insert Member\n2.Delete Member\n3.Delete Secretary\n4.Delete President\n5.Count members\n6.display\n7.reverse\n");
-        printf("Enter your choice:");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1:insertM(head);
-                   break;
-            case 2:deleteM(head);
-                 break;
-            case 3:deleteS(head);
-                  printf("Secretary node deleted.");
-                   break;
-            case 4:deleteP(head);
-                   printf("President node deleted.");
-                   break;
-            case 5:printf("Total members in group=%d",count(head));
-                  break;
-            case 6:reverse(head);
-                  break;
-             case 7:display(head);
-                    break;
-          }
-    }while(ch>0);
-   
-}
-void insertM(struct abc*head)
+void insertM()
 {
     struct abc * new,*temp;
     temp=head;
- new=(struct abc *)malloc(sizeof(struct abc));
+    new=(struct abc *)malloc(sizeof(struct abc));
     printf("Enter name:");
     scanf(" %s",new->name);
     printf("enter prn:");
@@ -95,77 +48,123 @@ void insertM(struct abc*head)
     new->prev=NULL;
     new->next=temp->next;
     temp->next=new;
+    new->next->prev=new;
     new->prev=temp;
     
-}
-void deleteP(struct abc* head)
-{
-    struct abc*temp;
-    temp=head->next;
-    free(head);
-}
-void deleteS(struct abc*head)
-{
-    struct abc*temp;
-    temp=head;
-    while(temp->next!=NULL)
-    {
-        temp=temp->next;
-    }
-    (temp->prev)->next=NULL;
-    free(temp);
-}
-void deleteM(struct abc*head)
-{
-    struct abc*p,*q;
-    char ch[10];
-    p=head;
-    printf("Enter member name to be deleted:");
-    scanf(" %s",ch);
-    while(ch!=p->name)
-    {
-        q=p;
-        p=p->next;
-    }
-    q->next=p->next;
-    (p->next)->prev=q;
-    p->prev=NULL;
-    p->next=NULL;
     
 }
-int count(struct abc*head)
+void deleteP()
+{
+    struct abc *temp;
+    temp=head;
+    free(head);
+    head=temp->next;
+    
+    printf("\nThe President has been deleted..\n");
+}
+void deleteS()
+{
+    struct abc*temp1,*temp2;
+    temp1=head;
+    while(temp1->next!=NULL)
+    {
+        temp1=temp1->next;
+    }
+    temp2=temp1->prev;
+    temp2->next=NULL;
+    free(temp1);
+}
+void deleteM()
+{
+    struct abc*temp1,*temp2;
+    int pr;
+    printf("Enter pr of member to be deleted:");
+    scanf("%d",&pr);
+    temp1=head;
+    while(temp1->prn!=pr)
+    {
+        temp1=temp1->next;
+    }
+    temp2=temp1->prev;
+    temp2->next=temp1->next;
+    temp1->next->prev=temp2;
+    free(temp1);
+    temp1=NULL;
+    
+    
+}
+int count()
 {
        struct abc *temp;
        temp=head;
-       int count;
-       while(temp->next!=NULL)
+       int count=0;
+       while(temp!=NULL)
        {
+              temp=temp->next;
               count++;
        }
        return count;
 }
-void reverse(struct abc *head)
+void reverse(struct abc*head)
 {
-   struct abc*current,*temp;
-
-    current = head;
-    while(current != NULL)
+    struct abc *temp;
+    temp=head;
+    if(temp==NULL)
+        printf("\nList Is Empty\n");    
+    else if(temp->next==NULL)
+        printf("Name=  %s\t PRN= %d\n",temp->name,temp->prn);
+    else
     {
-        temp = current->next;
-        current->next = current->prev;
-        current->prev = temp;
-        current = temp;
+        reverse(temp->next);
+        printf("Name=  %s\t PRN= %d\n",temp->name,temp->prn);
     }
-    if(temp->prev==NULL)
-    {
-       head=temp;
-    }
-printf("LIST REVERSED SUCCESSFULLY.\n");
-
 }
+void main()
+{
+    head=(struct abc*)malloc(sizeof(struct abc));
+    printf("enter name and PRN(President):\n");
+    scanf(" %s",head->name);
+    scanf("%d",&head->prn);
+    head->next=NULL;
+    head->prev=NULL;
 
+    inserts();
+    printf("\n");
+    int ch,y;
+    do{
+        printf("\n 1.Insert Member\n");
+        printf("2.Delete member\n");
+        printf("3.Delete President\n");
+        printf("4.Delete Secretary\n");
+        printf("5.Count Members\n");
+        printf("6.Reverse\n");
+        printf("7.Display\n");
 
-
-
-
+        printf("Enter your choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:insertM(head);
+                   break;
+            case 2:deleteM(head);
+                 break;
+            case 3:deleteP(head);
+                  printf("President node deleted.");
+                   break;
+            case 4:deleteS(head);
+                   printf("Secretary node deleted.");
+                   break;
+            case 5:printf("Total members in group=%d\n",count(head));
+                  break;
+            case 6:reverse(head);
+                 break;
+             case 7:display(head);
+                    break;
+             default:exit(1);
+        }
+    
+    printf("Do you want to continue(0/1):");
+    scanf("%d",&y);
+     } while(y==1);   
+}
 
