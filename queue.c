a39cb445886eb060fd79858518b87f101a2b2dd3
Pranavnarkhede <114733916+Pranavnarkhede@users.noreply.github.com>
2022-12-03 15:38:21 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,67 +1,78 @@
 #include<stdio.h>
-#define max 20
+#define max 5
 
-int front=-1,rear=-1;
+int front= -1,rear= -1;
 int s[max];
 void enqueue()
 {
     int a;
-    printf("enter element to be pushed");
+    printf("\nenter element to be pushed==>");
     scanf("%d",&a);
-    if((front ==0 && rear==max-1) || (rear+1)%max==front)
+    if((front == 0 && rear == (max-1)) || (rear+1)%max==front)
     {
-        printf("overflow");
+        printf("overflow\n");
     }
     else
     {
-        if(rear==max-1)
+      if (front == -1) 
+    {
+        front = rear = 0;
+        s[rear] = a;
+    }
+    else if (rear == max-1 && front != 0)
+    {
+        rear =(rear+1)%max;
+        s[rear] = a;
+    }
+   else
     {
         rear=(rear+1)%max;
-        s[rear]=a;
+        s[rear] = a;
     }
-    else
-    {
-        rear=(rear+1)%max;
-        s[rear]=a;
-    }
-    }
-  
-
+    
+ }
 }
 int dequeue()
 {
     int element;
   if(front==-1)
   {
-    printf("oveflow");
-  }
-  else{
-    element=s[front];
-    if(front==rear)
-    {
-     front=rear=-1;
-    }
-  else{
-    front=(front+1)%max;
-  }
-
-
-}
-}
-void Display()
-{
-    int i=0;
-  if(front=-1)
-  {
-    printf("queue is empty");
+    printf("overflow");
   }
   else
   {
-    {
-        printf("%d",s[i]);
-        i++;
-    }
+      element=s[front];
+      if(front == rear)
+      {
+     front=-1;
+     rear=-1;
+      }
+       if(front==max-1){
+    front=(front+1)%max;
+      }
+      else{
+            front=(front+1)%max;
+      }
   }
+  return element; 
+}
+void display()
+{
+  int i=0;
+  if(front==-1)
+  {
+    printf("\nqueue is empty");
+  }
+  else
+  {
+     printf("Elements in Queue:");
+     for (i = front; i != rear; i = (i + 1) % max)
+        {
+            printf("%d ",s[i]);
+        }
+    printf("%d \n", s[i]);
+    }
+  
 }
 
 int main()
@@ -71,19 +82,21 @@ int main()
         printf("\n1.Insert");
         printf("\n1.Delete");
         printf("\n1.Display");
+        printf("\nEnter your choice:");
         scanf("%d",&ch);
         switch(ch)
         {
             case 1:enqueue();
                 break;
-            case 2:printf("Element deleted is:%d",dequeue());
+            case 2:printf("\nElement deleted is:%d",dequeue());
                 break;
-            case 3:Display();
+            case 3:display();
                 break;    
 
         }
-        printf("do you want to continue:");
+        printf("\ndo you want to continue:");
         scanf("%d",&y);
     }while(y==1);
+
+    return 0;
 } 
- 
